c2010d078390b0fc2c3aef8b1a9502c468ec1b52
Carlos Filipe de Castro Lemos <ethoshomo@gmail.com>
2023-02-18 17:32:27 -0300

    Script de Automização

---
+++ Projeto2/projeto2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /tmp
+apt update -y && apt upgrade -y
+apt install apache2 -y
+apt install unzip -y
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cp -r linux-site-dio-heads-main/* /var/www/html
+
